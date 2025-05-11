638c356a8e6170def8ffd99b47b2cf710d96f9f4
pratyusha2020 <92961548+pratyusha2020@users.noreply.github.com>
2025-05-11 11:42:11 -0700

    Add files via upload

---
+++ ACT1.py
@@ -0,0 +1,7 @@
+user= input("Enter a word")
+for i in user:
+    if (i=="a"):
+        print("a is found")
+        break
+    else:
+        print("a is not found")
---
+++ ACT3.py
@@ -0,0 +1,8 @@
+var=11
+while var>0:
+    var=var-1
+    if var==5:
+        continue
+    print("\ncurrent variable value is", var)
+
+print("\n Good Bye!")
